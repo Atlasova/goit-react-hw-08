c32eabd21efd57c567223b64f51093987c69f45a
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-11-13 14:31:42 +0100

    text homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,15 @@ import css from './HomePage.module.css';
 const HomePage = () => {
   return (
     <>
-      <h1 className={css.title}>Welcome to your personal Contact Book!</h1>
+      <h1 className={css.title}>
+        Hello! Welcome to your personal Contact Book!
+      </h1>
       <p className={css.subtitle}>
-        This site helps you conveniently store and organize your contacts.
+        This contact book is designed to help you keep track of your personal
+        and professional connections. It is a place to store important details
+        and organize your contacts.
       </p>
-      <p className={css.subtitle}>To get started, Register or Login</p>
+      <p className={css.subtitle}>To get started, Register or Log in...</p>
     </>
   );
 };
